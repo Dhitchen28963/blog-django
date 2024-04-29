4f9b0e61de2856201cac8cfb3b36baa06cbe31b7
David Hitchen <hitchen28963@yahoo.com>
2024-04-29 17:10:25 +0000

    Added superuser log in and secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4z2)!96(siyvk&5dz=^3jr*f84$*1n_&$oc&mtv8oc2n2nqhp&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
